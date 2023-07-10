0b83783625d352341cf2450d8808fc8fafd0366f
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-10 20:04:22 +0300

    CommonJS require

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {MyMath} = require('./MyMath.js');
+const {Component} = require('./Component.js');
+
+const sum2n2 = MyMath.sum(2, 2);
+console.log(sum2n2);
+const multy3n5 = MyMath.multy(3, 5);
+console.log(multy3n5);
+
+const component = new Component();
+console.log(component.render());
