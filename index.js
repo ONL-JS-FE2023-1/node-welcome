88cf2c5c0d0b6ba531853ba7e8da18ffafd2abe7
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-10 20:45:03 +0300

    How require works?

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ console.log(multy3n5);
 
 const component = new Component();
 console.log(component.render());
+
+/*
+require(path)
+
+HOW REQUIRE WORKS
+
+resolving -> loading -> wrappening -> evaluation -> caching
+
+resolving - шукає той файл, який ми вказали у шляху
+loading - читання знайденого файлу
+wrappening - огортання в нодівський контекст
+evaluation - виконання коду
+caching - зберігаємо результат роботи модулю
+
+RESOLVING:
+    1) Core modules
+    2) File
+        *.js || *.json
+    3) Directory
+        3.1) package.json -> "main"
+        3.2) index.js || index.json
+    4) node_modules
+    5) throw new Error()
+
+*/
