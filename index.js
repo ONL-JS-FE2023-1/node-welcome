34dfba4ab4c184288eaac7f815bfbf58b3cf6f9a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-12 19:49:28 +0300

    Webpack

--- index.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash');
-
-const number = _.random(1, 10, false);
-
-console.log(number);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+
+const div = document.querySelector('#root');
+div.append('HELLO, WEBPACK! TEXT TEST!!!! MOBILE HI!!!!');
+
+
+console.log('INDEX START');
+console.log(_.random(1, 10, false));
+console.log('INDEX END');
